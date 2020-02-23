9f275f3a30b0315d8197edae04ce4ce716a4f8a6
gemaderus <gderus@paradigmadigital.com>
2020-02-23 10:21:10 +0100

    Added test autocomplete

--- src/hc-hooks/useAutocomplete/useAutocomplete.test.js
+++ src/hc-hooks/useAutocomplete/useAutocomplete.test.js
@@ -2,44 +2,53 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import useAutocomplete from './useAutocomplete';
 
 test('should change input value', () => {
-  const { result } = renderHook(() => useAutocomplete());
+  const { result } = renderHook(() => useAutocomplete(['casa', 'house']));
+
+  expect(result.current.value).toBe('');
+  expect(result.current.filter).toEqual([]);
 
   act(() => {
-    result.current.onInput('house');
+     result.current.onInput('house');
+   })
+
+   expect(result.current.value).toBe('house');
+})
+
+test('should filter', () => {
+  const { result } = renderHook(() => useAutocomplete(['casa', 'house', 'masa']));
+
+  act(() => {
+     result.current.onInput('sa');
+   })
+
+   expect(result.current.filter).toEqual(['casa', 'masa']);
+})
+
+
+test('should not filter if input is empty', () => {
+  const { result } = renderHook(() => useAutocomplete(['casa', 'house', 'masa']));
+
+  act(() => {
+     result.current.onInput('sa');
+   })
+
+   expect(result.current.filter).toEqual(['casa', 'masa']);
+
+   act(() => {
+    result.current.onInput('');
   })
 
-  expect(result.current.value).toBe('house');
-  expect(result.current.filtering).toBe(true);
+  expect(result.current.filter).toEqual([]);
 })
 
 test('should selected a value', () => {
-  const { result } = renderHook(() => useAutocomplete());
+  const { result } = renderHook(() => useAutocomplete((['casa', 'house', 'masa'])));
 
   act(() => {
-    result.current.onSelect('milk');
+    result.current.onSelect('masa');
   })
 
-  expect(result.current.value).toBe('milk');
-  expect(result.current.filtering).toBe(false);
+  expect(result.current.value).toBe('masa');
+  expect(result.current.filter).toEqual([]);
 })
-
-test('should return a result value if it finds the value into the data', () => {
-  const { result } = renderHook(() => useAutocomplete(['house', 'rose', 'roseton']));
-
-  act(() => {
-    result.current.filterValue('rose');
-  })
-
-  expect(result.current.filter).toBe(['rose', 'roseton']);
-})
-
-test('if the value not matches with data, it should return a empty array ', () => {
-  const { result } = renderHook(() => useAutocomplete(['house', 'rose', 'roseton']));
-
-  act(() => {
-    result.current.filterValue('paper');
-  })
-
-  expect(result.current.filter).toBe([]);
-});
 
