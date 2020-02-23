9ac525d9f5221a97b2288066cf74e7c684776817
gemaderus <gderus@paradigmadigital.com>
2020-02-23 10:21:56 +0100

    Added test slider and toggle

--- src/hc-hooks/useSlider/useSlider.test.js
+++ src/hc-hooks/useSlider/useSlider.test.js
@@ -1,32 +1,28 @@
 import { renderHook, act } from '@testing-library/react-hooks';
-import useToggle from './useToggle';
-import { useState } from 'react';
 import useSlider from './useSlider';
 
 describe('useState test', () => {
   test('should use useSlider', () => {
-    const { result } = renderHook(() => useSlider());
-    const [value] = result.current.active;
+    const { result } = renderHook(() => useSlider(0));
+    const value = result.current.active;
 
     expect(value).toBe(0);
   });
 
   test('should use useSlider', () => {
     const { result } = renderHook(() => useSlider(1));
-    const [value] = result.current.active;
+    const value = result.current.active;
 
     expect(value).toBe(1);
   });
 
-  test('should update setToggle value using setter', () => {
-    const { result } = renderHook(() => useState());
-
-    const [_, setActive] = result.current;
-
-    act(() => setActive(3))
-
-    const [value] = result.current.active;
-
-    expect(value).toBe(3);
+  test('should update the value', () => {
+    const { result } = renderHook(() => useSlider(2))
+    const { setActive } = result.current;
+    
+    act(() => setActive(2))
+    
+    expect(result.current.active).toBe(2)
   })
 });
+
--- src/hc-hooks/useToggle/useToggle.test.js
+++ src/hc-hooks/useToggle/useToggle.test.js
@@ -1,12 +1,11 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import useToggle from './useToggle';
-import { useState } from 'react';
 
 describe('useState tests', () => {
   test('should use toggle', () => {
     const { result } = renderHook(() => useToggle());
     
-    const [value] = result.current.toggled; 
+    const value = result.current.toggled; 
   
     expect(value).toBe(false)
   });
@@ -14,20 +13,17 @@ describe('useState tests', () => {
   test('should use toggle', () => {
     const { result } = renderHook(() => useToggle(true));
     
-    const [value] = result.current.toggled; 
+    const value = result.current.toggled; 
   
     expect(value).toBe(true)
   });
   
-  test('should update setToggle value using setter', () => {
-    const { result } = renderHook(() => useState(true))
-  
-    const [_, setValue] = result.current;
-  
-    act(() => setValue(false))
-  
-    const [value] = result.current.toggled;
-  
-    expect(value).toBe(false)
+  test('should update toggle value', () => {
+    const { result } = renderHook(() => useToggle(true))
+    const {setToggle} = result.current;
+
+    act(() => setToggle(false))
+
+    expect(result.current.toggled).toBe(false)
   });
 });
