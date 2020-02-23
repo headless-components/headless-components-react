bfa13a453b03153e4e25eb75d6b0c7037c644d53
gemaderus <gderus@paradigmadigital.com>
2020-02-23 10:21:24 +0100

    Added test slider

--- src/hc-hooks/usePagination/usePagination.test.js
+++ src/hc-hooks/usePagination/usePagination.test.js
@@ -1,93 +1,45 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 
-import usePagination from './usePagination';
+import { usePagination } from './usePagination';
 
 describe('usePagination test', () => {
   test('should return the currentPage', () => {
-    const { result } = renderHook(() => usePagination({totalItems: 150, itemsPerPage: 10, window: 4, initialPage: 1}))
+    const { result } = renderHook(() => usePagination({ totalItems: 150, itemsPerPage: 10, window: 4, initialPage: 1}))
   
-    const [value] = result.current.currentPage;
+    const value = result.current.currentPage;
   
     expect(value).toBe(1);
   });
 
-  test('should update the active page', () => {
-    const { result } = renderHook(() => usePagination());
-  
-    const [_, updatePage] = result.current;
-  
-    act(() => updatePage(2))
-  
-    const [value] = result.current.currentPage;
-  
-    expect(value).toBe(2);
+  test('should update active page', () => {
+    const { result } = renderHook(() => usePagination({ totalItems: 150, itemsPerPage: 10, window: 4, initialPage: 1}))
+
+    const { updatePage } = result.current;
+
+    act(() => updatePage(3))
+
+    expect(result.current.currentPage).toBe(3)
   });
 
+  test('should render total pages', () => {
+    const { result } = renderHook(() => usePagination({ totalItems: 150, itemsPerPage: 10, window: 4, initialPage: 1}))
 
+    expect(result.current.totalPages).toBe(15)
+  });
 
-  // test('should return the total pages', () => {
-  //   const { result } = renderHook(() => usePagination({totalItems: 150, itemsPerPage: 10}));
+  test('should render range pages', () => {
+    const { result } = renderHook(() => usePagination({ totalItems: 150, itemsPerPage: 10, window: 3, initialPage: 1}))
 
-  //   const [value] = result.current.totalPages;
+    expect(result.current.rangePages).toEqual([1, 2, 3])
 
-  //   expect(value).toBe()
-  // })
-
-  // test('should return range page', () => {
-  //   const { result } = renderHook(() => usePagination());
-  
-  //   const [_, updatePage] = result.current;
-  
-  //   act(() => updatePage(2))
-  
-  //   const [value] = result.current.currentPage;
-  
-  //   expect(value).toBe(2);
-  // });
-  
-})
-
-useEffect(() => {
-  setTotalPages(Math.ceil(totalItems / itemsPerPage));
-}, [totalItems, itemsPerPage]);
-
-describe('useEffects tests', () => {
-  test('should handle useEffect hook', () => {
-    const sideEffect = {[1]: false, [2]: false}
-
-    const { rendered, unmount } = renderHook(({ totalItems, itemsPerPage }) => {
-      useEffect(() => {
-        sideEffect[totalItems] = true;
-        sideEffect[itemsPerPage] = true;
-        return () => {
-          sideEffect[totalItems] = false;
-          sideEffect[itemsPerPage] = true;
-        }
-      }, ['totalItems', 'itemsPerPage'])
-  },
-  { initialProps: { totalItems: 150, itemsPerPage: 10 } }
-  )
-
-  expect(sideEffect[150, 10]).toBe(true)
-  expect(sideEffect[0, 1]).toBe(false)
-
-  rendered({pages: 15});
+    act(() => result.current.updatePage(6))
+    expect(result.current.rangePages).toEqual([5, 6, 7])
+  });
 });
 
 
-// let range = Array.from(Array(endPage + 1 - startPage).keys()).map(
-//   i => startPage + i
-// );
 
 
 
 
 
-
-
-
-
-currentPage: active,
-    updatePage: setActive,
-    rangePages: range,
-    totalPages: pages
