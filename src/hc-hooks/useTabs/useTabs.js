45269fda84f0dab6e12b1b071885c829f9fa91fc
gemaderus <gderus@paradigmadigital.com>
2020-02-23 12:17:31 +0100

    Fixed useTabs and added js test

--- src/hc-hooks/useTabs/useTabs.js
+++ src/hc-hooks/useTabs/useTabs.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 
 function useTabs(tabs, initialTab) {
-  const labels = tabs.map(l => l.label);
 
+  const labels = useMemo(() => tabs.map(l => l.label), [tabs]);
   const makeMenu = (labels, current) => {
     return labels.map((labelText, index) => ({
       label: labelText,
       active: index === current
     }));
   };
-
+    
   let [active, setActive] = useState(initialTab);
   let [activeContent, setActiveContent] = useState(tabs[initialTab].content);
   let [menu, setMenu] = useState(makeMenu(labels, initialTab));
@@ -17,7 +17,8 @@ function useTabs(tabs, initialTab) {
   useEffect(() => {
     setActiveContent(tabs[active].content);
     setMenu(makeMenu(labels, active));
-  }, [active, labels, tabs]);
+  }, [active]); //eslint-disable-line
+
   return { Content: activeContent, tabsList: menu, updateTabs: setActive };
 }
 
